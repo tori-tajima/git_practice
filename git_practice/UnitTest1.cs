8da9974d7eafbf70758e9330d48e35533f779849
h-tajima <h-tajima@torishima.co.jp>
2024-06-05 10:10:49 +0900

    プロジェクト ファイルを追加します。

---
+++ git_practice/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace git_practice
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
